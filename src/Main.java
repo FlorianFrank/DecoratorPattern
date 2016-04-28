162f9b7099ee21c38c1d97f9a7b4509c7a726d97
FlorianFrank <ffrank@hm.edu>
2016-04-28 11:44:27 +0200

    NaryCounter erstellt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,31 @@
+import edu.hm.ffrank.arch.decorator_pattern.LoopCounter;
+import edu.hm.ffrank.arch.decorator_pattern.NaryCounter;
+
 /**
  * Created by florianfrank on 28.04.16.
  */
 public class Main {
     public static void main(String[] args){
+        NaryCounter loopCounter = new NaryCounter(9);
+        System.out.println(loopCounter.tick().read());
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+        System.out.println(loopCounter.tick().read());
+
+        System.out.println(loopCounter.tick().read());
+
+
+
 
     }
 }
