4b7fbe8a1a007292527b2f2ee23f5b60508f4efc
FlorianFrank <ffrank@hm.edu>
2016-04-29 13:44:23 +0200

    Tests für LoopCounter,Narycounter

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
-import edu.hm.ffrank.arch.decorator_pattern.ClockSecondCounter;
-import edu.hm.ffrank.arch.decorator_pattern.LoopCounter;
-import edu.hm.ffrank.arch.decorator_pattern.NaryCounter;
-import edu.hm.ffrank.arch.decorator_pattern.PrintCounter;
+import edu.hm.ffrank.arch.decorator_pattern.*;
 
 /**
  * Created by florianfrank on 28.04.16.
  */
 public class Main {
     public static void main(String[] args){
-    NaryCounter naryCounter = new NaryCounter(3);
-        PrintCounter printCounter = new PrintCounter(naryCounter,'\n');
-        printCounter.tick().tick().tick();
+    NaryCounter naryCounter = new NaryCounter(9);
+       for(int i=0;i<10;i++){
+           System.out.println(naryCounter.read());
+           naryCounter.tick();
+       }
+
+
+
 
 
 
